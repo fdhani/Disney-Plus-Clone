b7dcdd6df4ae276acb1a7065b862a97d3ce921b2
Fachran Dhani Achmad <fachran@99.co>
2024-06-12 23:36:18 +0700

    add swiper

--- src/components/ScrollableSection/ScrollableSection.tsx
+++ src/components/ScrollableSection/ScrollableSection.tsx
@@ -1,8 +1,14 @@
 import { PropsWithChildren } from "react";
+import styles from "./ScrollableSection.module.css";
 
 const ScrollableSection = (props: PropsWithChildren<unknown>) => {
   const { children } = props;
-  return <div className="scrollable-section">{children}</div>;
+  return (
+    <div>
+      <h2 className={styles.sectionTitle}>Section Name</h2>
+      {children}
+    </div>
+  );
 };
 
 export default ScrollableSection;
