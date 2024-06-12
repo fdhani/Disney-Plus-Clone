f3f0ea3013780116a191b40f1d61cba334d5e71c
Fachran Dhani Achmad <fachran@99.co>
2024-06-12 23:36:18 +0700

    add swiper

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import BannerDetail from "../../components/BannerDetail";
 import BannerMask from "../../components/BannerMask";
 import VideoBanner from "../../components/VideoBanner";
 import MediaCard from "../../components/MediaCard";
+import ScrollableSection from "../../components/ScrollableSection/ScrollableSection";
 
 const Home = () => {
   return (
@@ -19,17 +20,15 @@ const Home = () => {
         releaseDate="2022-01-01"
         language="English"
       />
-      <div
-        style={{
-          width: "200px",
-        }}
-      >
-        <MediaCard
-          title="Inside Out"
-          posterImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
-          bannerImage="https://image.tmdb.org/t/p/w342/uVu2fBc114un7F1GD76RBouWyBP.jpg"
-          description="Growing up can be a bumpy road, and it's no exception for Riley, who is uprooted from her Midwest life when her father starts a new job in San Francisco. Like all of us, Riley is guided by her emotions - Joy, Fear, Anger, Disgust and Sadness. The emotions live in Headquarters, the control center inside Riley's mind, where they help advise her through everyday life. As Riley and her emotions struggle to adjust to a new life in San Francisco, turmoil ensues in Headquarters. Although Joy, Riley's main and most important emotion, tries to keep things positive, the emotions conflict on how best to navigate a new city, house and school."
-        />
+      <div>
+        <ScrollableSection>
+          <MediaCard
+            title="Inside Out"
+            posterImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
+            bannerImage="https://image.tmdb.org/t/p/w342/uVu2fBc114un7F1GD76RBouWyBP.jpg"
+            description="Growing up can be a bumpy road, and it's no exception for Riley, who is uprooted from her Midwest life when her father starts a new job in San Francisco. Like all of us, Riley is guided by her emotions - Joy, Fear, Anger, Disgust and Sadness. The emotions live in Headquarters, the control center inside Riley's mind, where they help advise her through everyday life. As Riley and her emotions struggle to adjust to a new life in San Francisco, turmoil ensues in Headquarters. Although Joy, Riley's main and most important emotion, tries to keep things positive, the emotions conflict on how best to navigate a new city, house and school."
+          />
+        </ScrollableSection>
       </div>
     </div>
   );
