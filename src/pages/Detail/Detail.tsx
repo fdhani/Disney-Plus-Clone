cae977153e32d4c4f10e60e037efd5703e5c26bf
Fachran Dhani Achmad <fachran@99.co>
2024-06-13 14:12:58 +0700

    step-5/implement-router

---
+++ src/pages/Detail/Detail.tsx
@@ -0,0 +1,15 @@
+const Detail = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "white",
+        }}
+      >
+        DETAIL PAGE PLACEHOLDER
+      </h1>
+    </div>
+  );
+};
+
+export default Detail;
