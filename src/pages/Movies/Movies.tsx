3361306dab5aa98df7f08fdc9f581d8a3a143940
Fachran Dhani Achmad <fachran@99.co>
2024-06-13 21:43:25 +0700

    add more pages

---
+++ src/pages/Movies/Movies.tsx
@@ -0,0 +1,15 @@
+const Movies = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "white",
+        }}
+      >
+        MOVIES PAGE PLACEHOLDER
+      </h1>
+    </div>
+  );
+};
+
+export default Movies;
