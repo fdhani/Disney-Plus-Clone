e98dd3c8a72542fcb44b8ae2b3d285e47414af1e
Fachran Dhani Achmad <fachran@99.co>
2024-06-13 14:12:58 +0700

    step-5/implement-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:type/:id",
+    element: <Detail />,
+  },
+]);
 
 function App() {
   return (
     <Layout>
-      <Home />
+      <RouterProvider router={router} />
     </Layout>
   );
 }
