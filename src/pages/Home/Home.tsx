10e97eb36ef63f1f4f8f25be993c98d0495a3f45
Fachran Dhani Achmad <fachran@99.co>
2024-06-14 13:19:34 +0700

    move files

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import BannerDetail from "./components/BannerDetail";
-import BannerMask from "./components/BannerMask";
-import VideoBanner from "./components/VideoBanner";
+import BannerDetail from "../../components/BannerDetail";
+import BannerMask from "../../components/BannerMask";
+import VideoBanner from "../../components/VideoBanner";
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       {/* THIRD LAYER */}
       <BannerDetail
         genres={["Action", "Adventure", "Fantasy"]}
-        title="Acolyte"
+        title="Rise of the planet of the apes"
         overview="NEW EPISODE EVERY WEDNESDAY. “The Acolyte” examines a galaxy of
             secrets and dark-side powers, and all is not what it seems."
         releaseDate="2022-01-01"
